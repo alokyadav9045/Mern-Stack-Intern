734f13eb7d5592ad3d5f9eefd52e51e445e89fbb
Alok Yadav <163675739+alokyadav9045@users.noreply.github.com>
2024-12-23 15:38:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1>Responsive Layout Using CSS Grid </h1>
+          <img src={logo} className="App-logo" alt="logo" />
+        </p>
+        <p>
+          <h1> Learn React </h1>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
